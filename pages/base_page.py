63e67ebc27acd1c8730d65c5952d2f6986315030
buj17 <igor.ssobolev.06@gmail.com>
2026-03-12 15:07:31 +0300

    Added login link checking to test_guest_can_go_to_login_page; Added is_element_present method to BasePage

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,21 @@
+from selenium.common import NoSuchElementException
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import ByType
 
 
 class BasePage:
-    def __init__(self, browser: WebDriver, url: str):
+    def __init__(self, browser: WebDriver, url: str, timeout: int = 10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how: ByType, what: str):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+
+        return True
